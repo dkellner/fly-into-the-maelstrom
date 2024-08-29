6ea255908bb87fbb088741ba55125e74f57bd1f4
Dominik Kellner <dkellner@dkellner.de>
2024-08-29 16:19:43 +0200

    Implement challenge #2: Unique ID generation

--- justfile
+++ justfile
@@ -11,5 +11,17 @@ test:
 
 maelstrom-echo:
     cargo build --bin echo && \
-    maelstrom test -w echo --bin "$CARGO_TARGET_DIR/debug/echo" \
-      --node-count 1 --time-limit 10
+    maelstrom test -w echo \
+      --bin "$CARGO_TARGET_DIR/debug/echo" \
+      --node-count 1 \
+      --time-limit 10
+
+maelstrom-unique-ids:
+    cargo build --bin unique-ids && \
+    maelstrom test -w unique-ids \
+      --bin "$CARGO_TARGET_DIR/debug/unique-ids" \
+      --time-limit 30 \
+      --rate 1000 \
+      --node-count 3 \
+      --availability total \
+      --nemesis partition
---
+++ src/bin/unique_ids.rs
@@ -0,0 +1,82 @@
+use std::ops::RangeFrom;
+
+use fly_into_the_maelstrom::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(PartialEq, Eq, Clone, Copy, Debug, Serialize, Deserialize)]
+struct UniqueId(NodeId, u64);
+
+#[derive(PartialEq, Eq, Clone, Debug, Serialize, Deserialize)]
+struct GenerateBody {
+    msg_id: MessageId,
+}
+
+#[derive(PartialEq, Eq, Clone, Debug, Serialize, Deserialize)]
+struct GenerateOkBody {
+    msg_id: MessageId,
+    in_reply_to: MessageId,
+    id: UniqueId,
+}
+
+#[derive(PartialEq, Eq, Clone, Debug, Serialize, Deserialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
+enum RequestBody {
+    Generate(GenerateBody),
+}
+
+#[derive(PartialEq, Eq, Clone, Debug, Serialize, Deserialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
+enum ResponseBody {
+    GenerateOk(GenerateOkBody),
+}
+
+#[derive(Debug)]
+struct UniqueIdsNode {
+    id: NodeId,
+    msg_ids: MessageIdIter,
+    internal_ids: RangeFrom<u64>,
+}
+
+impl UniqueIdsNode {
+    fn next_unique_id(&mut self) -> UniqueId {
+        UniqueId(
+            self.id,
+            self.internal_ids
+                .next()
+                .expect("exhausted available internal ids"),
+        )
+    }
+}
+
+impl InitializedNode for UniqueIdsNode {
+    type RequestBody = RequestBody;
+    type ResponseBody = ResponseBody;
+
+    fn new(id: NodeId, _all_nodes: Box<[NodeId]>) -> Self {
+        Self {
+            id,
+            msg_ids: MessageIdIter::default(),
+            internal_ids: 0..,
+        }
+    }
+
+    fn handle(&mut self, request: Message<RequestBody>) -> Option<Message<ResponseBody>> {
+        let RequestBody::Generate(body) = request.body;
+        Some(Message {
+            src: self.id,
+            dest: request.src,
+            body: ResponseBody::GenerateOk(GenerateOkBody {
+                msg_id: self
+                    .msg_ids
+                    .next()
+                    .expect("exhausted available message ids"),
+                in_reply_to: body.msg_id,
+                id: self.next_unique_id(),
+            }),
+        })
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    run_node::<UniqueIdsNode>()
+}
